0247d76bd52c59543e201e97cc87c2fa106e794c
whike-chan <10iridescent.symphogear01@gmail.com>
2022-10-18 16:18:25 +0900

    Update フォルダ整理

--- README.md
+++ README.md
@@ -9,7 +9,9 @@
 画像フォルダーを監視し、変更されると画像圧縮が走ります。
 
 ## 対象ファイル
-画像圧縮処理の対象と出力後のパスは `imagemin.mjs` で指定しています。  
 png/jpg/gif/svgに対応しています。  
+画像圧縮処理の対象と出力後のパスは `imagemin.mjs` で指定しています。  
 
 監視対象の画像フォルダーは `package.json` の `scripts` 内 `img:watch` に指定しています。
+
+初期設定では `_src/assets/img/` 内の `jpg,jpeg,png,svg,gif` が処理対象で、出力先は `./assets/img/` になっています。
--- imagemin.mjs
+++ imagemin.mjs
@@ -7,40 +7,41 @@ import svgo from 'imagemin-svgo'
 (async () => {
   try {
     console.time('imagemin');
+
     // 処理したい画像を渡す
-    await imagemin(['_resource/assets/img/**/*.{jpg,jpeg,png,svg,gif}'], {
-        destination: 'build/images',
-        plugins: [
-          // png
-          pngquant({
-            quality: [.65, .8],
-            speed: 1
-          }),
-          // jpg
-          mozjpeg({
-            quality: 80
-          }),
-          // gif
-          gifsicle(),
-          // svg
-          svgo({
-            plugins: [{
-              name: 'preset-default',
-              params: {
-                overrides: {
-                  removeViewBox: false,
-                  convertShapeToPath: false,
-                  collapseGroups: false,
-                  cleanupIDs: false
-                }
+    await imagemin(['_src/assets/img/**/*.{jpg,jpeg,png,svg,gif}'], {
+      destination: 'build/images',
+      plugins: [
+        // png
+        pngquant({
+          quality: [.65, .8],
+          speed: 1
+        }),
+        // jpg
+        mozjpeg({
+          quality: 80
+        }),
+        // gif
+        gifsicle(),
+        // svg
+        svgo({
+          plugins: [{
+            name: 'preset-default',
+            params: {
+              overrides: {
+                removeViewBox: false,
+                convertShapeToPath: false,
+                collapseGroups: false,
+                cleanupIDs: false
               }
-            }]
-          })
-        ],
-        // 出力先
-        replaceOutputDir: output => {
-          return output.replace(/_resource\/assets\/img\//, './html/assets/img/')
-        }
+            }
+          }]
+        })
+      ],
+      // 出力先指定
+      replaceOutputDir: output => {
+        return output.replace(/_src\/assets\/img\//, './assets/img/')
+      }
     });
     console.timeEnd('imagemin');
   }
